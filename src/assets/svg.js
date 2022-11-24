01cb8ece69931d0dd8306e3d9e96182ccea61faa
Mahmudjon <mahmidjonasqarov101@gmail.com>
2022-11-24 17:28:16 +0500

    fox

---
+++ src/assets/svg.js
@@ -0,0 +1,10 @@
+export const Logo = () => {
+    return (
+        <svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="60.000000pt" height="72.000000pt" viewBox="0 0 53.000000 72.000000" preserveAspectRatio="xMidYMid meet"> <metadata> Created by potrace 1.16, written by Peter Selinger 2001-2019 </metadata> <g fill="#05fdd8" stroke="none"> <path d="M380 442 c0 -318 -1 -322 -71 -322 -61 0 -89 40 -89 126 l0 45 -67
+        -3 -68 -3 3 -69 c6 -158 87 -225 256 -214 80 5 134 38 165 100 20 42 21 59 21
+        331 l0 287 -75 0 -75 0 0 -278z"></path> </g> <g transform="translate(0.000000,72.000000) scale(0.100000,-0.100000)" fill="#fd2155" stroke="none"> <path d="M380 442 c0 -318 -1 -322 -71 -322 -61 0 -89 40 -89 126 l0 45 -67
+        -3 -68 -3 3 -69 c6 -158 87 -225 256 -214 80 5 134 38 165 100 20 42 21 59 21
+        331 l0 287 -75 0 -75 0 0 -278z"></path> </g> </svg>
+
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import "./index.css";
-import reducer from "./redux/reducer";
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>
+  </React.StrictMode>
 );
+
+
+
+// import React, { Suspense } from "react";
+// import Loader from "./utils/loader/Loader";
+// import ReactDOM from "react-dom/client";
+// import reportWebVitals from "./reportWebVitals";
+// import "./index.css";
+
+// const App = React.lazy(() => {
+//   return new Promise((resolve) => {
+//     setTimeout(() => resolve(import("./App")), 3800);
+//   });
+// });
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <Suspense fallback={<Loader />}>
+//       <App />
+//     </Suspense>
+//   </React.StrictMode>
+// );
+// reportWebVitals();
